a698d5f697cda4dfee7802f883d1e85e30c59013
Cairi-S <cairi.swainson@gmail.com>
2021-05-03 16:05:31 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -18,18 +18,17 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated by
-    commas.  The loop will repeatedly request data, until it is valid.
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print("Please enter sales data from the last market ")
-        print("Data should be six numbers, seperated by commas")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
-        validate_data(sales_data)
 
         if validate_data(sales_data):
             print("Data is valid!")
@@ -41,30 +40,30 @@ def get_sales_data():
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted to int,
-    or if there aren't exactly 6 values
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exact 6 values required, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-  
+
     return True
 
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet update successfully.\n")
+    print("Sales worksheet updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -75,10 +74,16 @@ def calculate_surplus_data(sales_row):
     - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
-    stock = SHEET.worksheet('stock').get_all_values()
+    stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
     
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 def main():
     """
@@ -87,8 +92,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
-print("Welcome to Love Sandwiches data automation")
+print("Welcome to Love Sandwiches Data Automation")
 main()
